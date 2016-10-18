733e94f485205dacf7205618746902f16bcd9dca
vlad <inge__garcia@hotmail.com>
2016-10-18 17:37:37 -0500

    terminado

---
+++ app/controllers/index.rb
@@ -0,0 +1,16 @@
+get '/' do
+  erb :index
+end
+
+get '/:new_word' do
+	@new_word = params[:new_word]
+
+  @words    = Word.anagrams_words(@new_word)
+
+  erb :word
+end
+
+post '/new_word' do
+  redirect to ("/#{params[:user_word]}")
+end
+
